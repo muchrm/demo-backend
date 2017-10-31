780b9f4d77ad6bf34f91f34e377bb4029a12ac14
Pongpanot Chuaysakun <muchrm.me@gmail.com>
2017-10-31 18:04:09 +0700

    add authen service

---
+++ authen/src/modules/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import {
+  MiddlewaresConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import * as passport from 'passport';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './passport/jwt.strategy';
+
+@Module({
+  components: [
+    AuthService,
+    JwtStrategy,
+  ],
+  controllers: [AuthController],
+})
+export class AuthModule implements NestModule {
+  public configure(consumer: MiddlewaresConsumer) {
+    consumer
+      .apply(passport.authenticate('jwt', { session: false }))
+      .forRoutes({ path: '/auth/authorized', method: RequestMethod.ALL });
+  }
+}
