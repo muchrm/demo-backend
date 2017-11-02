0c8aaafb2c302a185956c3e82d1468b7dcb39e10
Pongpanot Chuaysakun <muchrm.me@gmail.com>
2017-11-02 10:13:23 +0700

    แดงเพียบเลย ไม่รุ้ตัวไหนทำ

--- authen/src/modules/auth/auth.service.ts
+++ authen/src/modules/auth/auth.service.ts
@@ -9,38 +9,38 @@ export class AuthService {
     const expiresIn = 60 * 60;
     const secretOrKey = 'secret';
     const user = { email: 'thisis@example.com' };
-    const issuer = "http://mis.sci.buu.ac.th";
-    const token = jwt.sign(user, secretOrKey, {issuer, expiresIn });
-    await this.generateConsumer(user.email,issuer,secretOrKey)
+    const issuer = 'http://mis.sci.buu.ac.th';
+    const token = jwt.sign(user, secretOrKey, { issuer, expiresIn });
+    await this.generateConsumer(user.email, issuer, secretOrKey);
     return {
       expires_in: expiresIn,
       access_token: token,
     };
   }
 
-  async login(credential:any): Promise<any> {
+  async login(credential: any): Promise<any> {
     const expiresIn = 60 * 60;
     const secretOrKey = 'secret';
-    const user = { email: 'thisis@example.com',username:credential.username };
-    const issuer = "http://mis.sci.buu.ac.th";
+    const user = { email: 'thisis@example.com', username: credential.username };
+    const issuer = 'http://mis.sci.buu.ac.th';
 
-    const token = jwt.sign(user, secretOrKey, {issuer, expiresIn });
-    await this.generateConsumer(user.email,issuer,secretOrKey)
+    const token = jwt.sign(user, secretOrKey, { issuer, expiresIn });
+    await this.generateConsumer(user.email, issuer, secretOrKey);
 
     return {
       expires_in: expiresIn,
       access_token: token,
     };
   }
-  async generateConsumer(username:string,key:string,secret:string){
-    const result = await this.kong.get('/consumers/'+username)
-    if(result.message == 'Not found'){
-      await this.kong.post('/consumers/'+username,{username})
+  async generateConsumer(username: string, key: string, secret: string) {
+    const result = await this.kong.get('/consumers/' + username);
+    if (result.message === 'Not found') {
+      await this.kong.post('/consumers/' + username, { username });
     }
-    const jwts =  await this.kong.get('/consumers/'+username+'/jwt')
-    await jwts.data.forEach( async (jwt) => {
-      await this.kong.delete('/consumers/'+username+'/jwt/'+jwt.id);
+    const jwts = await this.kong.get('/consumers/' + username + '/jwt');
+    await jwts.data.forEach(async (token) => {
+      await this.kong.delete('/consumers/' + username + '/jwt/' + token.id);
     });
-    return await this.kong.post('/consumers/'+username+'/jwt',{key,secret})
+    return await this.kong.post('/consumers/' + username + '/jwt', { key, secret });
   }
 }
