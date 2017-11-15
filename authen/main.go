b487d2f214bedf0e7f6e257b8ecf54b6f84293e9
Pongpanot Chuaysakun <muchrm.me@gmail.com>
2017-11-15 17:43:58 +0700

    update authen

--- authen/main.go
+++ authen/main.go
@@ -13,9 +13,18 @@ import (
 	"github.com/muchrm/mis-backend/authen/kong"
 )
 
+type SessionTokenResponse struct {
+	Token string `json:"token"`
+}
 type User struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+func writeJSON(w http.ResponseWriter, status int, response interface{}) error {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(response)
 }
 
 func generateToken(username string, key string, secret string) (string, error) {
@@ -26,6 +35,7 @@ func generateToken(username string, key string, secret string) (string, error) {
 	})
 	return token.SignedString([]byte(secret))
 }
+
 func generateConsumer(username string, key string, secret string) {
 	kong, _ := kong.New("http://localhost:8001")
 	_, err := kong.Customer.Get(username)
@@ -37,6 +47,7 @@ func generateConsumer(username string, key string, secret string) {
 		kong.Jwt.Add(username, key, secret)
 	}
 }
+
 func LoginHandler(w http.ResponseWriter, req *http.Request) {
 	var user User
 	if req.Body == nil {
@@ -55,12 +66,13 @@ func LoginHandler(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, err.Error(), 400)
 		return
 	}
-
-	fmt.Fprint(w, "token="+token)
+	writeJSON(w, http.StatusOK, SessionTokenResponse{Token: token})
 }
+
 func HelthCheck(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(w, "Service Healthly")
 }
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/login", LoginHandler).Methods("POST")
